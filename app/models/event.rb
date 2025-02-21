8bd11b867d566801dc1d388dc6b72ffba8fa4852
Victor Da Luz <vdaluz@gmail.com>
2025-02-21 15:18:27 -0600

    Model setup

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,9 @@
 class Event < ApplicationRecord
+  has_many :users, through: :registrations
+
+  scope :upcoming, -> { where("start_time > ?", Time.now) }
+  scope :past, -> { where("end_time < ?", Time.now) }
+
+  validates :name, presence: true
+  validates :end_time, comparison: { greater_than: :start_time }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  enum :role, [ admin: "admin", atendee: "atendee" ]
+
+  validates :email, presence: true, uniqueness: true
+  validates :role, presence: true
+  validates :name, presence: true
+
+  has_many :events, through: :registrations
 end
