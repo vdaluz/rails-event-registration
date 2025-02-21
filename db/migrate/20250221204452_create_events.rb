c225ce6192bc3a16a36a14f0eb6ebee57a0c1a9b
Victor Da Luz <vdaluz@gmail.com>
2025-02-21 14:54:22 -0600

    Create db tables

--- db/migrate/20250221204452_create_events.rb
+++ db/migrate/20250221204452_create_events.rb
@@ -1,6 +1,10 @@
 class CreateEvents < ActiveRecord::Migration[8.0]
   def change
     create_table :events do |t|
+      t.string :name
+      t.datetime :start_time
+      t.datetime :end_time
+      t.string :location
       t.timestamps
     end
   end
--- db/migrate/20250221204458_create_registrations.rb
+++ db/migrate/20250221204458_create_registrations.rb
@@ -1,7 +1,11 @@
 class CreateRegistrations < ActiveRecord::Migration[8.0]
   def change
     create_table :registrations do |t|
+      t.integer :user_id
+      t.integer :event_id
       t.timestamps
+
+      t.index [ :user_id, :event_id ], unique: true
     end
   end
 end
